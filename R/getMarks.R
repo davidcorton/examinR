9d6f5730fc4a46bcd857d87509cadade508a5c2f
davidcorton <david.orton@gmail.com>
2022-01-18 11:58:01 +0000

    Added simple mode to getMarks, for quick analysis of single data sheets

--- R/getMarks.R
+++ R/getMarks.R
@@ -1,26 +1,29 @@
-getMarks <- function(marks, students, modules) {
+getMarks <- function(marks, students, modules, simple = F) {
 
   # Read in marks, plus look-up tables for students and modules
   marks <- data.table(read_sheet(marks))
-  students <- data.table(read_sheet(students))
-  modules <- data.table(read_sheet(modules))
 
-  # Merge the three tables together
-  data <- merge(marks, students, by = "Student")
-  data <- merge(data, modules, by = "Code")
+  if(simple == F) {
+    students <- data.table(read_sheet(students))
+    modules <- data.table(read_sheet(modules))
 
-  # Make sure all the variables are the right types
+    # Merge the three tables together
+    data <- merge(marks, students, by = "Student")
+    data <- merge(data, modules, by = "Code")
+
+    # Specify SSP categories
+    data[is.na(SSP), SSP := "N"]
+    data[, SSP := as.factor(SSP)]
+
+    # Make sure all the variables are the right types
+    data[, Shortname := as.factor(Shortname)]
+    data[, Code := as.factor(Code)]
+    data[, Term := as.factor(Term)]
+    data[, Gender := as.factor(Gender)]
+    data[, Programme := as.factor(Programme)]
+  }
   data[, Module := as.factor(Module)]
-  data[, Shortname := as.factor(Shortname)]
-  data[, Code := as.factor(Code)]
-  data[, Term := as.factor(Term)]
   data[, Student := as.factor(Student)]
-  data[, Gender := as.factor(Gender)]
-  data[, Programme := as.factor(Programme)]
-
-  # Specify SSP categories
-  data[is.na(SSP), SSP := "N"]
-  data[, SSP := as.factor(SSP)]
 
   # If text has found its way into Mark (most likely as a note after the actual numbers), purge it
   if(!class(data$Mark) == "numeric") {
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(examinR)
+
+test_check("examinR")

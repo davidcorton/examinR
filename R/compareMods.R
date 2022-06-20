89facd5dec14e907b383c05d6ef8d4dfd93e7336
David Orton <david.orton@gmail.com>
2022-06-20 10:00:54 +0100

    Made upper limit changeable for boxplots

--- R/compareMods.R
+++ R/compareMods.R
@@ -1,4 +1,5 @@
-compareMods <- function(x, output.name = "boxplots.jpg", title = "Module marks", dimensions = c(1500, 1000), screen = F, ...) {
+compareMods <- function(x, output.name = "boxplots.jpg", title = "Module marks", dimensions = c(1500, 1000),
+                        upper = 90, screen = F, ...) {
 
   # Remove missing values and zero marks
   x <- x[!is.na(Mark) & !Mark == 0]
@@ -12,11 +13,13 @@ compareMods <- function(x, output.name = "boxplots.jpg", title = "Module marks",
   summ[, Mod := factor(summ$Shortname, levels = summ$Shortname)]
   x <- merge(x, summ[, list(Shortname, Mod)], by = "Shortname")
 
-  # Define margins and output device
+  # Set output device
   if(screen == F) {
     jpeg(output.name, dimensions[1], dimensions[2], quality = 100)
-    par(mar = c(17, 6, 4, 1), oma = c(0, 0, 0, 0), mfrow = c(1, 1))
   }
+
+  # Define margins
+  par(mar = c(17, 6, 4, 1), oma = c(0, 0, 0, 0), mfrow = c(1, 1))
 
   # Set markers
   colcodes <- c("darkgoldenrod", "darkblue", "grey")
@@ -26,7 +29,7 @@ compareMods <- function(x, output.name = "boxplots.jpg", title = "Module marks",
   with(x, boxplot(Mark ~ Mod, las = 2, outline = F,
                   cex.main = 2.5, cex = 3.5, cex.axis = 2, col = "grey", lwd = 3, cex.lab = 3,
                   main = paste0(title, " (KW: p = ", round(KW$p.value, 3), ")"),
-                  ylab = "", xlab = "", ylim = c(25, 90)))
+                  ylab = "", xlab = "", ylim = c(25, upper)))
   mtext("Module mark", side = 2, line = 4, cex = 3)
 
   # Add reference lines for common grade boundaries
